c93d1d3a91b57fddea1940ca0577a688edd45cea
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-01-20 17:09:58 +0100

    added cursor for viable TI contacts, added layout file for picker and added an appropriate title to the strings file.

--- PickContactsForTrustedIntroductionActivity.java
+++ PickContactsForTrustedIntroductionActivity.java
@@ -2,16 +2,12 @@ package org.thoughtcrime.securesms.trustedIntroductions;
 
 import org.thoughtcrime.securesms.ContactSelectionActivity;
 import org.thoughtcrime.securesms.ContactSelectionListFragment;
+import org.thoughtcrime.securesms.PushContactSelectionActivity;
 
 /**
  * Queries the Contacts Provider for Contacts which match strongly verified contacts in the Signal database,
  * and let's the user choose one for the purpose of carrying out a trusted introduction.
  */
-public class PickContactForTrustedIntroductionActivity extends ContactSelectionActivity
-    implements ContactSelectionListFragment.OnContactSelectedListener{
-
-
-  @Override public void onSelectionChanged() {
-
-  }
+public class PickContactsForTrustedIntroductionActivity extends PushContactSelectionActivity {
+  
 }
