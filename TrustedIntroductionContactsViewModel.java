949e496f6049654fc1b54386ce06a03888d283be
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-01-20 22:51:30 +0100

    non-syntax error state

--- TrustedIntroductionContactsViewModel.java
+++ TrustedIntroductionContactsViewModel.java
@@ -2,6 +2,13 @@ package org.thoughtcrime.securesms.trustedIntroductions;
 
 import androidx.lifecycle.ViewModel;
 
+
 public class TrustedIntroductionContactsViewModel extends ViewModel {
+
+  private final TrustedIntroductionContactManager manager;
+
+  private TrustedIntroductionContactsViewModel() {
+    this.manager = new TrustedIntroductionContactManager();
+  }
 
 }
