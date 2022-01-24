ef8f23dac56ae1104bc61dc7218965ab2961e264
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-01-24 15:53:12 +0100

    added nested factory to ContactsViewModel

--- TrustedIntroductionContactsViewModel.java
+++ TrustedIntroductionContactsViewModel.java
@@ -1,6 +1,13 @@
 package org.thoughtcrime.securesms.trustedIntroductions;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.thoughtcrime.securesms.groups.GroupId;
+import org.thoughtcrime.securesms.groups.ui.addmembers.AddMembersViewModel;
+
+import java.util.Objects;
 
 
 public class TrustedIntroductionContactsViewModel extends ViewModel {
@@ -9,6 +16,19 @@ public class TrustedIntroductionContactsViewModel extends ViewModel {
 
   private TrustedIntroductionContactsViewModel() {
     this.manager = new TrustedIntroductionContactManager();
+  }
+
+
+  public static class Factory implements ViewModelProvider.Factory {
+
+    public Factory(@NonNull GroupId groupId) {
+      
+    }
+
+    @Override
+    public @NonNull <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+      return Objects.requireNonNull(modelClass.cast(new TrustedIntroductionContactsViewModel()));
+    }
   }
 
 }
