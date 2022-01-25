8bd8428a72138afa634c8d9ce6ead2f3b57b12fb
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-01-25 13:48:31 +0100

    connected the cursor query from identity database in order to load valid TI contacts from recipient Database

--- TrustedIntroductionContactManager.java
+++ TrustedIntroductionContactManager.java
@@ -3,33 +3,55 @@ package org.thoughtcrime.securesms.trustedIntroductions;
 import android.content.Context;
 
 import androidx.annotation.WorkerThread;
+import androidx.core.util.Consumer;
 
+import org.signal.core.util.concurrent.SignalExecutors;
 import org.thoughtcrime.securesms.contacts.SelectedContact;
+import org.thoughtcrime.securesms.database.IdentityDatabase;
+import org.thoughtcrime.securesms.database.RecipientDatabase;
+import org.thoughtcrime.securesms.database.SignalDatabase;
 import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
+import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientId;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
 final class TrustedIntroductionContactManager {
 
   private final Context context;
-  // This is the person which will receive the security numbers of the selcted contacts through
+  // This is the person which will receive the security numbers of the selected contacts through
   //  a secure introduction.
-  private RecipientId recipientId;
+  private final RecipientId recipientId;
 
-  TrustedIntroductionContactManager(){
-    this.context = ApplicationDependencies.getApplication();
-    this.recipientId = null;
-  }
 
   TrustedIntroductionContactManager(RecipientId recipientId){
     this.context = ApplicationDependencies.getApplication();
     this.recipientId = recipientId;
   }
 
-  public void setRecipientId(RecipientId recipientId){
-    this.recipientId = recipientId;
+  void getValidContacts(@androidx.annotation.NonNull Consumer<List<Recipient>> introducableContacts){
+    SignalExecutors.BOUNDED.execute(() -> {
+      IdentityDatabase idb = SignalDatabase.identities();
+      RecipientDatabase rdb = SignalDatabase.recipients();
+      try(RecipientDatabase.RecipientReader reader = rdb.getReader(idb.getTIUnlocked())){
+        int count = reader.getCount();
+        if (count == 0){
+          introducableContacts.accept(Collections.emptyList());
+        } else {
+          List<Recipient> contacts = new ArrayList<>();
+          while(reader.getNext() != null){
+            contacts.add(reader.getCurrent());
+          }
+          introducableContacts.accept(contacts);
+        }
+      }
+    });
   }
+
 
   public RecipientId getRecipientId(){
     return this.recipientId;
