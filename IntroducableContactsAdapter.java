0e9369065f5a3bd582f82665a8782eacec7cff3c
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-02-11 15:10:29 +0100

    more seperation of concerns: removed some indirection through the adapter

--- IntroducableContactsAdapter.java
+++ IntroducableContactsAdapter.java
@@ -26,10 +26,8 @@ import java.util.Locale;
 
 /**
  * Adaptation of ContactSelectionListAdapter, CursorRecyclerViewAdapter and BlockedUsersAdapter.
- * Yes, horribly uggly. But was deemend preferable than extending the base code to my use-case, since
- * the code can be more cleanly decoupled this way.
  *
- * Holds the selection state.
+ * Presents the recipients and forwards the click logic to the Viewmodel.
  *
  */
 public class IntroducableContactsAdapter extends ListAdapter<Recipient, IntroducableContactsAdapter.ContactViewHolder> {
@@ -37,40 +35,10 @@ public class IntroducableContactsAdapter extends ListAdapter<Recipient, Introduc
   private final static String TAG = Log.tag(IntroducableContactsAdapter.class);
 
   private final @NonNull Context         context;
-  private @Nullable View    header;
-
 
   private final LayoutInflater                                layoutInflater;
   private final ContactSelectionListAdapter.ItemClickListener clickListener;
   private final GlideRequests    glideRequests;
-  private final TrustedIntroductionContactsViewModel viewModel;
-
-  public boolean isSelectedContact(@NonNull SelectedContact contact) {
-    return viewModel.isSelectedContact(contact);
-  }
-
-  public void addSelectedContact(@NonNull SelectedContact contact) {
-    if (!viewModel.addSelectedContact(contact)) {
-      Log.i(TAG, "Contact was already selected, possibly by another identifier");
-    }
-  }
-
-  public void removeFromSelectedContacts(@NonNull SelectedContact selectedContact) {
-    int removed = viewModel.removeFromSelectedContacts(selectedContact);
-    Log.i(TAG, String.format(Locale.US, "Removed %d selected contacts that matched", removed));
-  }
-
-  public @NonNull IntroducableContactsAdapter.ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return new ContactViewHolder(layoutInflater.inflate(R.layout.contact_selection_list_item, parent, false), clickListener);
-  }
-
-
-  @Override public void onBindViewHolder(@NonNull IntroducableContactsAdapter.ContactViewHolder holder, int position) {
-
-    Recipient current = getItem(position);
-    String name = current.getDisplayNameOrUsername(context.getApplicationContext());
-    holder.bind(glideRequests, current.getId(), 0, name, null, null, null, false);
-  }
 
   public IntroducableContactsAdapter(@NonNull Context context,
                                      @NonNull GlideRequests glideRequests,
@@ -82,21 +50,21 @@ public class IntroducableContactsAdapter extends ListAdapter<Recipient, Introduc
     this.layoutInflater  = LayoutInflater.from(context);
     this.glideRequests   = glideRequests;
     this.clickListener   = clickListener;
-    this.viewModel = viewModel;
   }
 
-  public List<SelectedContact> getSelectedContacts() {
-    return viewModel.listSelectedContacts();
+  public @NonNull IntroducableContactsAdapter.ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return new ContactViewHolder(layoutInflater.inflate(R.layout.contact_selection_list_item, parent, false), clickListener);
   }
 
-  public int getSelectedContactsCount() {
-    return viewModel.getSelectedContactsSize();
+  @Override public void onBindViewHolder(@NonNull IntroducableContactsAdapter.ContactViewHolder holder, int position) {
+    Recipient current = getItem(position);
+    String name = current.getDisplayNameOrUsername(context.getApplicationContext());
+    holder.bind(glideRequests, current.getId(), 0, name, null, null, null, false);
   }
-
 
   /**
    * Reusing classes from ContactSlectionListAdapter.
-   * Because the constructors are package private, they are duplicated here.
+   * Because the constructors are package private, they are duplicated & adapted here.
    */
   public abstract static class ViewHolder extends ContactSelectionListAdapter.ViewHolder {
 
@@ -122,9 +90,7 @@ public class IntroducableContactsAdapter extends ListAdapter<Recipient, Introduc
 
   }
 
-  public static class ContactViewHolder extends ViewHolder implements LetterHeaderDecoration.LetterHeaderItem {
-
-    private String letterHeader;
+  public static class ContactViewHolder extends ViewHolder{
 
     ContactViewHolder(@NonNull final View itemView,
                       @Nullable final ContactSelectionListAdapter.ItemClickListener clickListener)
@@ -162,16 +128,6 @@ public class IntroducableContactsAdapter extends ListAdapter<Recipient, Introduc
     public void setEnabled(boolean enabled) {
       getView().setEnabled(enabled);
     }
-
-    @Override
-    public @Nullable String getHeaderLetter() {
-      return letterHeader;
-    }
-
-    @Override
-    public void setLetterHeaderCharacter(@Nullable String letterHeaderCharacter) {
-      this.letterHeader = letterHeaderCharacter;
-    }
   }
 
   private static final class RecipientDiffCallback extends DiffUtil.ItemCallback<Recipient> {
@@ -185,34 +141,5 @@ public class IntroducableContactsAdapter extends ListAdapter<Recipient, Introduc
     public boolean areContentsTheSame(@NonNull Recipient oldItem, @NonNull Recipient newItem) {
       return oldItem.equals(newItem);
     }
-  }
-
-  private class AdapterDataSetObserver extends DataSetObserver {
-    @Override
-    public void onChanged() {
-      super.onChanged();
-    }
-
-    @Override
-    public void onInvalidated() {
-      super.onInvalidated();
-    }
-  }
-
-
-  public boolean hasHeaderView() {
-    return header != null;
-  }
-
-  public void setHeaderView(@Nullable View header) {
-    this.header = header;
-  }
-
-  public View getHeaderView() {
-    return this.header;
-  }
-
-  public interface ItemClickListener {
-    void onItemClick(ContactSelectionListItem item);
   }
 }
