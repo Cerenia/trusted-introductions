45c326076e00896194f25bf40e462aafb680f691
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-07-26 21:15:24 +0200

    added two more puzzlepieces to minimal example

---
+++ SelectedStrings.kt
@@ -0,0 +1,43 @@
+package org.thoughtcrime.securesms.trustedIntroductions
+
+import android.view.View
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.contacts.ContactChip
+import org.thoughtcrime.securesms.contacts.SelectedContacts
+import org.thoughtcrime.securesms.mms.GlideApp
+import org.thoughtcrime.securesms.util.adapter.mapping.LayoutFactory
+import org.thoughtcrime.securesms.util.adapter.mapping.MappingAdapter
+import org.thoughtcrime.securesms.util.adapter.mapping.MappingModel
+import org.thoughtcrime.securesms.util.adapter.mapping.MappingViewHolder
+
+object SelectedStrings {
+
+  @JvmStatic
+  fun register(adapter: MappingAdapter, onCloseIconClicked: (Model) -> Unit) {
+    adapter.registerFactory(Model::class.java, LayoutFactory({ ViewHolder(it, onCloseIconClicked) }, R.layout.contact_selection_list_chip))
+  }
+
+  class Model(val selectedString: MinimalStringModel, val str: String): MappingModel<Model> {
+    override fun areContentsTheSame(newItem: Model): Boolean {
+      return areItemsTheSame(newItem);
+    }
+
+    override fun areItemsTheSame(newItem: Model): Boolean {
+      return newItem.selectedString.getName().equals(str);
+    }
+  }
+
+  private class ViewHolder(itemView: View, private val onCloseIconClicked: (Model) -> Unit) : MappingViewHolder<Model>(itemView) {
+
+    private val chip: ContactChip = itemView.findViewById(R.id.contact_chip)
+
+    override fun bind(m: Model) {
+      chip.text = m.selectedString.get();
+      chip.setOnCloseIconClickListener {
+        onCloseIconClicked(m);
+      }
+    }
+
+  }
+
+}
