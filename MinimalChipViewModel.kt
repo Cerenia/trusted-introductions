1e7cfce6ded93e826259f011c93cfc1210676341
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-07-27 13:23:12 +0200

    List now correctly populated

--- MinimalChipViewModel.kt
+++ MinimalChipViewModel.kt
@@ -1,14 +1,7 @@
 package org.thoughtcrime.securesms.trustedIntroductions
 
 import androidx.lifecycle.ViewModel
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.kotlin.plusAssign
-import org.thoughtcrime.securesms.contacts.SelectedContacts
-import org.thoughtcrime.securesms.recipients.RecipientId
-import org.thoughtcrime.securesms.util.rx.RxStore
+
 
 class MinimalChipViewModel : ViewModel() {
 
