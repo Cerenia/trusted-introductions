4c5ccafea97eede69f98bda8b547a90effa54ef4
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-06 21:55:25 +0200

    finished the parsing and data initialization

--- TI_Data.kt
+++ TI_Data.kt
@@ -3,5 +3,5 @@ package org.thoughtcrime.securesms.trustedIntroductions
 import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase
 import org.thoughtcrime.securesms.recipients.RecipientId
 
-// introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database)
-data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceePhone: String, val introduceeIdentityKey: String, val predictedSecurityNumber: String, val timestamp: Long)
+// introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database) @see TI_Utils.encodeIdentityKey
+data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long)
