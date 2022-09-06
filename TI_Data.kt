5a3b3f17e6582f94b450914d70204d290083b840
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-06 12:28:13 +0200

    completed missing field in Object model, added more function scaffolding to Database

--- TI_Data.kt
+++ TI_Data.kt
@@ -1,7 +1,7 @@
 package org.thoughtcrime.securesms.trustedIntroductions
 
 import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase
-import java.sql.Timestamp
+import org.thoughtcrime.securesms.recipients.RecipientId
 
-
-data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val name: String, val phone: String, val identityKey: String, val securityNumber: String, val timestamp: Long)
+// introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database)
+data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?,val introduceeName: String, val introduceePhone: String, val introduceeIdentityKey: String, val predictedSecurityNumber: String, val timestamp: Long)
