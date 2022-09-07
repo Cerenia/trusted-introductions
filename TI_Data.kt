cda087b6e53a93689932a6c5e2964f5fe6ff5832
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-07 08:39:30 +0200

    Slight structural change in receive job + added hook for TI_Database

--- TI_Data.kt
+++ TI_Data.kt
@@ -2,6 +2,8 @@ package org.thoughtcrime.securesms.trustedIntroductions
 
 import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase
 import org.thoughtcrime.securesms.recipients.RecipientId
+import java.io.Serializable;
 
 // introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database) @see TI_Utils.encodeIdentityKey
-data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long)
+// TODO: if Java serialization does not work out of the box, use Json: https://kotlinlang.org/docs/serialization.html#example-json-serialization
+data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long) : Serializable
