80cb881400df1592913589e21e6f0e2bb427ea0b
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-07 14:47:54 +0200

    started building duplicate detection on incoming introduction and added a few todos where there are still uncertainties.

--- TI_Data.kt
+++ TI_Data.kt
@@ -6,4 +6,4 @@ import java.io.Serializable;
 
 // introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database) @see TI_Utils.encodeIdentityKey
 // TODO: if Java serialization does not work out of the box, use Json: https://kotlinlang.org/docs/serialization.html#example-json-serialization
-data class TI_Data (val id: String?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long) : Serializable
+data class TI_Data (val id: Long?, val state: TrustedIntroductionsDatabase.State?, val introducerId: RecipientId, val introduceeId: RecipientId?, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long) : Serializable
