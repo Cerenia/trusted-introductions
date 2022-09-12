46cf438ad2b2ed0301100d4c540f07ebe905bdbc
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-12 19:40:45 +0200

    some more activity design

--- TI_ManageActivity.java
+++ TI_ManageActivity.java
@@ -1,7 +1,15 @@
 package org.thoughtcrime.securesms.trustedIntroductions;
 
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
 import org.signal.core.util.logging.Log;
 import org.thoughtcrime.securesms.PassphraseRequiredActivity;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.recipients.RecipientId;
 import org.thoughtcrime.securesms.util.DynamicNoActionBarTheme;
 import org.thoughtcrime.securesms.util.DynamicTheme;
 
@@ -16,11 +24,35 @@ public class TI_ManageActivity extends PassphraseRequiredActivity {
 
   // String
   public static final String INTRODUCER_ID                 = "recipient_id";
-  public static final String SELECTED_CONTACTS_TO_FORWARD = "forwarding_contacts";
   // Instead of passing the RecipientID of the introducer as a string.
   public static final String ALL_INTRODUCTIONS = "ALL";
 
+  // TODO: Define all the views
+  private Toolbar toolbar;
+
+
   // TODO: may want an action bar for button
-  private final DynamicTheme dynamicTheme = new DynamicNoActionBarTheme();
+  private final DynamicTheme dynamicTheme = new DynamicTheme();
+
+  public static @NonNull Intent createIntent(@NonNull Context context, @NonNull RecipientId id){
+    Intent intent = new Intent(context, TI_ContactsChooseActivity.class);
+    intent.putExtra(INTRODUCER_ID, id.toString());
+    return intent;
+  }
+
+  private void initializeToolbar() {
+    setSupportActionBar(toolbar);
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    getSupportActionBar().setIcon(null);
+    getSupportActionBar().setLogo(null);
+    toolbar.setNavigationIcon(R.drawable.ic_arrow_left_24);
+    toolbar.setNavigationOnClickListener(v -> {
+      setResult(RESULT_CANCELED);
+      finish();
+    });
+  }
+
+  // TODO: Grab all the views & initialize in on_create
 
 }
