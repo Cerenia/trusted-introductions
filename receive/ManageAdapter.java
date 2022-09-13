a069e1b6f81d9d40d8091541c365d4f9140f0702
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-13 18:16:08 +0200

    more receiving UI

--- receive/ManageAdapter.java
+++ receive/ManageAdapter.java
@@ -1,5 +1,33 @@
 package org.thoughtcrime.securesms.trustedIntroductions.receive;
 
-public class ManageAdapter {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
+import org.thoughtcrime.securesms.trustedIntroductions.send.ContactsSelectionListItem;
+
+public class ManageAdapter extends ListAdapter<TI_Data, ManageAdapter.IntroductionViewHolder> {
+
+  static class IntroductionViewHolder extends RecyclerView.ViewHolder {
+
+    public IntroductionViewHolder(@NonNull View itemView, @NonNull final ManageAdapter.ItemClickListener clickListener) {
+      super(itemView);
+      itemView.setOnClickListener(v -> {
+        clickListener.onItemClick(getView());
+      });
+    }
+
+    ManageListItem getView() {
+      return (ManageListItem) itemView;
+    }
+  }
+
+  public interface ItemClickListener {
+    void onItemClick(ManageListItem item);
+  }
 
 }
--- receive/ManageListFragment.java
+++ receive/ManageListFragment.java
@@ -2,9 +2,18 @@ package org.thoughtcrime.securesms.trustedIntroductions.receive;
 
 import com.pnikosis.materialishprogress.ProgressWheel;
 
+import org.thoughtcrime.securesms.trustedIntroductions.send.ContactsSelectionAdapter;
+import org.thoughtcrime.securesms.trustedIntroductions.send.ContactsSelectionListItem;
+
 public class ManageListFragment {
 
   // TODO: Will probably need that for all screen
   private ProgressWheel showIntroductionsProgress;
 
+  private class IntroductionClickListener implements ContactsSelectionAdapter.ItemClickListener {
+
+    @Override public void onItemClick(ContactsSelectionListItem item) {
+      // TODO:
+    }
+  }
 }
