f1866978af51896af3355e0b0a41a12ba26340d8
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-09-14 10:41:52 +0200

    most of manage adapter written

--- receive/ManageListItem.java
+++ receive/ManageListItem.java
@@ -9,6 +9,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
+import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase;
+import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase.State;
 import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.util.ViewUtil;
@@ -18,15 +20,15 @@ import java.util.Date;
 
 public class ManageListItem extends ConstraintLayout {
 
-  private TextView dateView;
   private TI_Data        data;
+
+  private TextView dateView;
   private TextView       nameView;
   private TextView numberView;
-  // TODO: also add number
   private                                   SwitchMaterial   yn;
-  @SuppressLint("SimpleDateFormat") private final SimpleDateFormat datePattern = new SimpleDateFormat("yyyy/MM/dd h:mm:ss");
+  @SuppressLint("SimpleDateFormat") private final SimpleDateFormat datePattern = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
 
-  ManageListItem(@NonNull Context context, TI_Data d){
+  ManageListItem(@NonNull Context context, @NonNull TI_Data d){
     super(context);
     data = d;
   }
@@ -49,7 +51,87 @@ public class ManageListItem extends ConstraintLayout {
     // This will duplicate number in case there is no name, but that's just cosmetics.
     nameView.setText(data.getIntroduceeName());
     numberView.setText(data.getIntroduceeNumber());
+    // TODO: how to gray out whole thing if state is stale?
   }
 
+  public long getIntroductionId(){
+    return data.getId();
+  }
+
+  public TI_Data toggleSwitch(){
+    TI_Data newIntro;
+    TrustedIntroductionsDatabase.State newState;
+    switch(data.getState()){
+      case PENDING:
+      case REJECTED:
+        newState = State.ACCEPTED;
+        newIntro = changeState(data, newState);
+        setSwitchByState(newState);
+        break;
+      case ACCEPTED:
+        newState = State.REJECTED;
+        newIntro = changeState(data, newState);
+        setSwitchByState(newState);
+        break;
+      default:
+        // Do nothing if stale or conflicting
+        newIntro = data;
+    }
+    data = newIntro;
+    return newIntro;
+  }
+
+  private TI_Data changeState(TI_Data d, TrustedIntroductionsDatabase.State s){
+    return new TI_Data(d.getId(), s, d.getIntroducerId(), d.getIntroduceeId(), d.getIntroduceeServiceId(), d.getIntroduceeName(), d.getIntroduceeNumber(), d.getIntroduceeIdentityKey(), d.getPredictedSecurityNumber(), d.getTimestamp());
+  }
+
+  private void setSwitchByState(TrustedIntroductionsDatabase.State s){
+    // TODO: add "grey out" of complete list item to Stale states
+    // TODO: some visual indication of conflicting??
+    switch(s){
+      case PENDING:
+        yn.setText(R.string.ManageIntroductionsListItem__Pending);
+        yn.setChecked(false);
+        yn.setClickable(true);
+        break;
+      case ACCEPTED:
+        yn.setText(R.string.ManageIntroductionsListItem__Accepted);
+        yn.setChecked(true);
+        yn.setClickable(true);
+        break;
+      case REJECTED:
+        yn.setText(R.string.ManageIntroductionsListItem__Rejected);
+        yn.setChecked(false);
+        yn.setClickable(true);
+        break;
+      case CONFLICTING:
+        yn.setText(R.string.ManageIntroductionsListItem__Conflicting);
+        yn.setChecked(false);
+        yn.setClickable(false);
+        break;
+      case STALE_ACCEPTED:
+        yn.setChecked(true);
+        yn.setText(R.string.ManageIntroductionsListItem__Accepted);
+        yn.setClickable(false);
+        break;
+      case STALE_REJECTED:
+        yn.setChecked(false);
+        yn.setText(R.string.ManageIntroductionsListItem__Rejected);
+        yn.setClickable(false);
+        break;
+      case STALE_PENDING:
+        yn.setChecked(false);
+        yn.setText(R.string.ManageIntroductionsListItem__Stale);
+        yn.setClickable(false);
+        break;
+      case STALE_CONFLICTING:
+        yn.setChecked(false);
+        yn.setText(R.string.ManageIntroductionsListItem__Conflicting);
+        yn.setClickable(false);
+        break;
+    }
+  }
+
+  // TODO: Listener Interface for long press and short press (delete) which triggers actions in viewModel
 
 }
