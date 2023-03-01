b5015e23fad063b200db4b0ee1ad64f0ce90ffa6
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-03-01 12:56:28 +0100

    State no longer nullable

--- TI_Data.kt
+++ TI_Data.kt
@@ -10,9 +10,9 @@ import java.util.UUID
 
 // TODO: predictedSecurityNumber only needs to be nullable because I parse the TI_Message somewhat awkardly... maybe change at some point? Not super critical...
 // introduceeIdentityKey is encoded in Base64 (this is how it is currently stored in the Identity Database) @see TI_Utils.encodeIdentityKey
-data class TI_Data (val id: Long?, val state: TrustedIntroductionsDatabase.State?, val introducerServiceId: String, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long){
+data class TI_Data (val id: Long?, val state: TrustedIntroductionsDatabase.State, val introducerServiceId: String, val introduceeServiceId: String, val introduceeName: String, val introduceeNumber: String, val introduceeIdentityKey: String, var predictedSecurityNumber: String?, val timestamp: Long){
 
-  fun getIntroducerRecipientId(): RecipientId? {
+  public fun getIntroducerRecipientId(): RecipientId? {
     val validIntroducerId = UuidUtil.parse(introducerServiceId)
     var recipientId: RecipientId? = null
     if (validIntroducerId.isPresent)
@@ -25,9 +25,7 @@ data class TI_Data (val id: Long?, val state: TrustedIntroductionsDatabase.State
     val builder = JSONObject()
     // does nothing iff id == null see: https://developer.android.com/reference/kotlin/org/json/JSONObject
     builder.putOpt("id", id)
-    if (state != null){
-      builder.putOpt("state", state.toInt())
-    }
+    builder.put("state", state.toInt())
     builder.put("introducerServiceId", introducerServiceId)
     builder.put("introduceeServiceId", introduceeServiceId)
     builder.put("introduceeName", introduceeName)
@@ -49,12 +47,7 @@ data class TI_Data (val id: Long?, val state: TrustedIntroductionsDatabase.State
       } else{
         id = null
       }
-      val state: TrustedIntroductionsDatabase.State?
-      if (d.has("state")){
-        state = TrustedIntroductionsDatabase.State.forState(d.getInt("state"))
-      } else{
-        state = null
-      }
+      val  state = TrustedIntroductionsDatabase.State.forState(d.getInt("state"))
       val introducerServiceId = d.getString("introducerServiceId")
       val introduceeServiceId = d.getString("introduceeServiceId")
       val introduceeName = d.getString("introduceeName")
