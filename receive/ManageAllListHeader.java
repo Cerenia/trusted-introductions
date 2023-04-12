fb7475ae5b55add96965ba42c39f453f346b84e0
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-04-12 18:05:47 +0200

    header as constraint layout, currently not linking

---
+++ receive/ManageAllListHeader.java
@@ -0,0 +1,18 @@
+package org.thoughtcrime.securesms.trustedIntroductions.receive;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class ManageAllListHeader extends ConstraintLayout {
+
+  public ManageAllListHeader(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public ManageAllListHeader(@NonNull Context context) {
+    super(context);
+  }
+}
