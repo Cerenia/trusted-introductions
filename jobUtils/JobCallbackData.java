b6f0ce92f9700213043966a8046efd2cb8de644d
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-04-24 16:40:16 +0200

    interface better suited for TI_Serialize

--- jobUtils/JobCallbackData.java
+++ jobUtils/JobCallbackData.java
@@ -2,6 +2,6 @@ package org.thoughtcrime.securesms.trustedIntroductions.jobUtils;
 
 import org.thoughtcrime.securesms.jobs.TrustedIntroductionsRetreiveIdentityJob;
 
-public abstract class JobCallbackData extends TI_Serialize {
+public abstract class JobCallbackData implements TI_Serialize {
   public abstract TrustedIntroductionsRetreiveIdentityJob.TI_RetrieveIDJobResult getRetrieveIdJobStruct();
 }
