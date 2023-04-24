417644fae3047b354843d8f3e5f980ef988903a7
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-04-24 16:38:50 +0200

    no more factory member

--- jobUtils/TI_Serialize.java
+++ jobUtils/TI_Serialize.java
@@ -1,9 +1,6 @@
 package org.thoughtcrime.securesms.trustedIntroductions.jobUtils;
 
 import org.json.JSONException;
-import org.signal.core.util.logging.Log;
-import org.thoughtcrime.securesms.database.TrustedIntroductionsDatabase;
-import org.thoughtcrime.securesms.jobs.TrustedIntroductionsRetreiveIdentityJob;
 import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
 
 import java.util.HashMap;
@@ -22,6 +19,4 @@ public abstract class TI_Serialize {
    */
   abstract public TI_Serialize deserialize(String serialized) throws JSONException;
   abstract public TI_Data getIntroduction();
-
-  public TI_JobCallback.Factory factory = null;
 }
