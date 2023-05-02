46d83a6c98d711da351e19ac0b84d9106f2e0d84
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-05-02 10:26:42 +0200

    more cleanup in job hierarchy

--- jobUtils/TI_JobCallbackData.java
+++ jobUtils/TI_JobCallbackData.java
@@ -1,7 +1,9 @@
 package org.thoughtcrime.securesms.trustedIntroductions.jobUtils;
 
 import org.thoughtcrime.securesms.jobs.TrustedIntroductionsRetreiveIdentityJob;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
 
 public abstract class TI_JobCallbackData implements TI_Serialize {
-  public abstract TrustedIntroductionsRetreiveIdentityJob.TI_RetrieveIDJobResult getRetrieveIdJobStruct();
+  public abstract TrustedIntroductionsRetreiveIdentityJob.TI_RetrieveIDJobResult getIdentityResult();
+  abstract public TI_Data getIntroduction();
 }
