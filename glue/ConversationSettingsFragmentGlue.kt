8fece9c939c43443d85ab5a36acb4c968bbd45a7
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-07-07 12:37:43 +0200

    marked oneliners in AttachmentKeyboard and company, factored out navigation addition in ConversationSettingsFragment

---
+++ glue/ConversationSettingsFragmentGlue.kt
@@ -0,0 +1,42 @@
+package org.thoughtcrime.securesms.trustedIntroductions.glue
+
+import android.content.Context
+import android.content.Intent
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.components.settings.DSLSettingsIcon
+import org.thoughtcrime.securesms.components.settings.DSLSettingsText
+import org.thoughtcrime.securesms.components.settings.conversation.ConversationSettingsState
+import org.thoughtcrime.securesms.trustedIntroductions.receive.ManageActivity
+
+interface ConversationSettingsFragmentGlue {
+
+  companion object{
+    fun addTrustedIntroductionNavigation(state: ConversationSettingsState,
+                                         context: Context,
+                                         clickPref: (
+                                           title: DSLSettingsText,
+                                           summary: DSLSettingsText?,
+                                           icon: DSLSettingsIcon?,
+                                           iconEnd: DSLSettingsIcon?,
+                                           isEnabled: Boolean,
+                                           onClick: () -> Unit,
+                                           onLongClick: (() -> Boolean)?
+                                         ) -> Unit,
+                                         startActivity: (intent: Intent) -> Unit){
+      // Trusted Introductions
+      if (!state.recipient.isReleaseNotes && !state.recipient.isSelf){
+        clickPref(
+          DSLSettingsText.from(R.string.ConversationSettingsFragment__Introductions),
+          null,
+          DSLSettingsIcon.from(R.drawable.ic_trusted_introduction),
+          null,
+          true,
+          {
+            startActivity(ManageActivity.createIntent(context, ManageActivity.ActiveTab.NEW))
+          },
+          null
+        )
+      }
+    }
+  }
+}
