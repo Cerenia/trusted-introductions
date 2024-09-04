9206c02c8c6c99e8b323274151ba9ef4659a006d
Cerenia <christelle.gloor@inf.ethz.ch>
2024-09-04 14:43:29 +0200

    moved where TI messages are processed

---
+++ glue/MessageTableGlue.java
@@ -0,0 +1,19 @@
+package org.thoughtcrime.securesms.trustedIntroductions.glue;
+
+import org.thoughtcrime.securesms.database.MessageType;
+import org.thoughtcrime.securesms.mms.IncomingMessage;
+import org.thoughtcrime.securesms.recipients.RecipientId;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Utils;
+
+public interface MessageTableGlue {
+  static void handleTIMessage(IncomingMessage retreived){
+    // Group introductions are not supported.
+    if( !(retreived.getType() == MessageType.NORMAL) || retreived.isGroupMessage()) return;
+    String message = retreived.getBody();
+    if (message == null) return;
+    RecipientId introducer = retreived.getFrom();
+    long timestamp = retreived.getReceivedTimeMillis();
+    TI_Utils.handleTIMessage(introducer, message, timestamp);
+
+  }
+}
